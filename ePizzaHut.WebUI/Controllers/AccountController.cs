4dfbc1ffd2d067fe874b6f92fe6ee01b2d8b8bfe
khaungnaw <besamkhaungnaw1995@gmail.com>
2021-10-24 16:56:29 +0800

    added.

---
+++ ePizzaHut.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using ePizzaHut.Entity;
+using ePizzaHut.Services.Interfaces;
+using ePizzaHut.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ePizzaHut.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthentationService _authentationService;
+        public AccountController(IAuthentationService authentationService)
+        {
+            _authentationService = authentationService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User()
+                {
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber
+                };
+                var created = _authentationService.CreateUser(user, model.Password);
+                if (created)
+                {
+                    RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ ePizzaHut.WebUI/Models/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ePizzaHut.WebUI.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Password is requred")]
+        public string Password { get; set; }
+        
+        [Compare("Password"),Required(ErrorMessage = "Compare password do not match")]
+        public string ConfirmationPassword { get; set; }
+
+        [Required(ErrorMessage = "Phonen Number is required")]
+        public string PhoneNumber { get; set; }
+    }
+}
