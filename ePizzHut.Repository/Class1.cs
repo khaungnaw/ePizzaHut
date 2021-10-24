4293b63ad52abe335fd86d9222a749ae44619836
yadanarphyo <50695969+yadanarphyo@users.noreply.github.com>
2021-10-24 10:42:57 +0800

    first commit

---
+++ ePizzHut.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ePizzHut.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ ePizzaHut.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ePizzaHut.Services
+{
+    public class Class1
+    {
+    }
+}
